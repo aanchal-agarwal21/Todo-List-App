fcd5e0c62178501d1862f71830487b882aed6dbe
Aanchal <aanchal.agarwal@thoughtworks.com>
2022-07-04 07:59:12 +0530

    Add . CSS for todo-completed item

--- src/Components /TodoList/TodoList.js
+++ src/Components /TodoList/TodoList.js
@@ -5,18 +5,16 @@ import EditIcon from "@mui/icons-material/Edit";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 
 const TodoList = ({ todos, setTodos }) => {
-
-    const handleComplete =(todo)=>{
-        setTodos(
-            todos.map((item) =>{
-                if(item.id === todo.id)
-                {
-                    return{...item , completed:!item.completed};
-                }
-                return item;
-            })
-        )
-    };
+	const handleComplete = (todo) => {
+		setTodos(
+			todos.map((item) => {
+				if (item.id === todo.id) {
+					return { ...item, completed: !item.completed };
+				}
+				return item;
+			})
+		);
+	};
 
 	const handleDelete = ({ id }) => {
 		setTodos(todos.filter((todo) => todo.id !== id));
@@ -29,16 +27,16 @@ const TodoList = ({ todos, setTodos }) => {
 					<input
 						type="text"
 						value={todo.title}
-						className="todo-item"
+						className={todo.completed ? "todo-item completed" : "todo-item"}
 						onChange={(event) => event.preventDefault()}
 					/>
-					<button className="button check" onClick={()=>handleComplete(todo)}>
+					<button className="button check" onClick={() => handleComplete(todo)}>
 						<CheckIcon />
 					</button>
 					<button className="button edit">
 						<EditIcon />
 					</button>
-					<button className="button delete" onClick={()=>handleDelete(todo)}>
+					<button className="button delete" onClick={() => handleDelete(todo)}>
 						<DeleteForeverIcon />
 					</button>
 				</li>
